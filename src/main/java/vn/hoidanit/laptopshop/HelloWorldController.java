e5e3fef4df02f2b4c47f3df590eee0d4c3c12700
Trần Văn Hải Lâm <hailamtranvan@gmail.com>
2025-02-07 00:13:31 +0700

    Hello world with Spring Boot

---
+++ src/main/java/vn/hoidanit/laptopshop/HelloWorldController.java
@@ -0,0 +1,13 @@
+package vn.hoidanit.laptopshop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World from Hai Lam!";
+    }
+
+}

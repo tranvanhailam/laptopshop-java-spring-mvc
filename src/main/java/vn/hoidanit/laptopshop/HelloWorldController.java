c62eb211af5386e09d7e0438cc1630045bce29f0
Trần Văn Hải Lâm <hailamtranvan@gmail.com>
2025-02-08 18:29:23 +0700

    Set up spring-boot-devtools, connect database, spring-boot-starter-security

--- src/main/java/vn/hoidanit/laptopshop/HelloWorldController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloWorldController.java
@@ -7,7 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/")
     public String index() {
-        return "Hello World from Hai Lam!";
+        return "Hello World from Hai Lam Tr!";
     }
 
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page!";
+    }
 }
---
+++ src/main/java/vn/hoidanit/laptopshop/Services/MyService.java
@@ -0,0 +1,8 @@
+package vn.hoidanit.laptopshop.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+}
